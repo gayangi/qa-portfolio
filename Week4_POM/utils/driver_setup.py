07c13ba7c7ccb56f6929a8658db5152686963a60
gayangi <gayangidanangika1@gmail.com>
2025-12-06 11:52:07 +0100

    Update driver_setup.py

--- Week4_POM/utils/driver_setup.py
+++ Week4_POM/utils/driver_setup.py
@@ -1,8 +1,26 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# def get_driver():
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#     driver.maximize_window()
+#     return driver
+
+
+from selenium.webdriver.chrome.options import Options
 
 def get_driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = Options()
+    options.add_argument("--headless=new")  # for GitHub Actions
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
     driver.maximize_window()
     return driver
+
